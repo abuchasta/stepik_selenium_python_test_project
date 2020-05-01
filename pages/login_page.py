7449e50bdce62392adbbf6f4fcc4d4189ce47aa5
anastasiia <abuchasta@gmail.com>
2020-05-01 22:32:17 +0300

    Login_page.py file and locators for it were added

---
+++ pages/login_page.py
@@ -0,0 +1,18 @@
+﻿from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        current_link = self.browser.current_url.text
+        assert "login" in current_link, "Wrong link"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*MainPageLocators.REGISTRATION_FORM), "Registration form is not presented"
