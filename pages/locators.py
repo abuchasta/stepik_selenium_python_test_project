282135c5b084103cc51cb8dd6cb54c7eb8b18a47
anastasiia <abuchasta@gmail.com>
2020-05-05 23:13:21 +0300

    methods for login link were moved to BasePage file

--- pages/locators.py
+++ pages/locators.py
@@ -14,3 +14,7 @@ class ProductPageLocators():
     PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main p.price_color")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages .alert-success")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
